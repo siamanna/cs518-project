7eb5338ca0ffd9c4014053607aaf34b84291192d
Hannah Marsh <hrm1065@unh.edu>
2023-04-04 09:30:32 -0400

    finished intial web app

--- Project/WebApp/app.py
+++ Project/WebApp/app.py
@@ -1,19 +1,33 @@
 from flask import Flask, render_template
 import requests
-from bson import json_util
+import json
 
-app = Flask(__name__)
+
+username = "cs518"
+password = "ltgDm6reWuHh2m5E"
+auth_token = f"{username}:{password}"
 create_url = "https://dataservice1234.azurewebsites.net/api/createrecord"
 read_url = "https://dataservice1234.azurewebsites.net/api/readrecord"
+search_query = {}
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {auth_token}"
+}
+
+
+app = Flask(__name__, template_folder='/templates')
 
 
 @app.route('/records')  # create a new route for /records endpoint
 def records():
+
     # send a GET request to read record API
-    response = requests.get(read_url)
+    response = requests.get(read_url, params=search_query, headers=headers)
 
     # parse the JSON response into a Python list of dictionaries
-    records = json_util.loads(response.text)
+    records_dict = json.loads(response.content)
 
     # render the records.html template and pass in the records list as a variable
-    return render_template('records.html', records=records)
+    return render_template('records.html', records=records_dict)
+
